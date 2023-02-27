7fc4306ccacf55cd7e665b2210031f7bfb4ffa12
David Cap <david.cap@bocians.net>
2023-02-26 22:24:07 -0700

    add translations to Page module

---
+++ Resources/lang/cs/pages.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'pages' => 'Stránky',
+    'create page' => 'Vytvořit stránku',
+    'edit page' => 'Upravit stránku',
+    'name' => 'Název',
+    'tree' => 'Strom',
+
+    'slug' => 'Slug',
+    'title' => 'Titulek',
+
+    'meta_data' => 'Meta data',
+    'meta_title' => 'Meta titulek',
+    'meta_description' => 'Meta popis',
+    'facebook_data' => 'Facebook (OG) data',
+    'og_title' => 'Facebook (OG) titulek',
+    'og_description' => 'Facebook (OG) popis',
+    'og_type' => 'Facebook (OG) typ',
+    'template' => 'Název šablony stránky',
+    'is homepage' => 'Homepage ?',
+    'body' => 'Obsah',
+    'status' => 'Je dostupná na frontendu?',
+    'live' => 'Stránka je zveřejněna na webu',
+    'pages were updated' => 'Stránka byla aktualizována',
+
+    'back to index' => 'Zpět na seznam stránek',
+    'list resource' => 'Seznam stránek',
+    'create resource' => 'Vytvořit stránk',
+    'edit resource' => 'Upravit stránku',
+    'destroy resource' => 'Smazat stránku',
+
+    'view-page' => 'Zobrazit stránku',
+    'edit-page' => 'Upravit stránku',
+    'validation' => [
+        'attributes' => [
+            'title' => 'title',
+            'body' => 'body',
+        ],
+    ],
+    'facebook-types' => [
+        'website' => 'Website',
+        'product' => 'Produkt',
+        'article' => 'Článek',
+    ],
+];
